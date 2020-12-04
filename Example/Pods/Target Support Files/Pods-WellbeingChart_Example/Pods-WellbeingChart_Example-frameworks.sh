cc255a9fd030dac8edea713e2df11403907f778d
radohecko <radoslavhecko@gmail.com>
2020-12-04 11:06:10 +0100

    Pods

--- Example/Pods/Target Support Files/Pods-WellbeingChart_Example/Pods-WellbeingChart_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-WellbeingChart_Example/Pods-WellbeingChart_Example-frameworks.sh
@@ -197,9 +197,13 @@ if [ -r "${ARTIFACT_LIST_FILE}" ]; then
 fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Charts/Charts.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/TinyConstraints/TinyConstraints.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/WellbeingChart/WellbeingChart.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Charts/Charts.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/TinyConstraints/TinyConstraints.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/WellbeingChart/WellbeingChart.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
