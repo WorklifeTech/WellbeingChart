a345e4b8e9f8f08df2fea7ed3608f35c66995d09
radohecko <radoslavhecko@gmail.com>
2020-12-04 10:58:44 +0100

    UIFont extension to dynamically load custom fonts

---
+++ WellbeingChart/Classes/WellbeingChartFont.swift
@@ -0,0 +1,34 @@
+//
+//  WellbeingChartFont.swift
+//  WellbeingChart
+//
+//  Created by Rado Hečko on 04/12/2020.
+//
+
+import Foundation
+
+// Source: https://stackoverflow.com/questions/12919513/how-do-i-add-a-custom-font-to-a-cocoapod
+extension UIFont {
+    static func registerFont(bundle: Bundle, fontName: String, fontExtension: String) -> Bool {
+        guard let fontURL = bundle.url(forResource: fontName, withExtension: fontExtension) else {
+            fatalError("Couldn't find font \(fontName)")
+        }
+
+        guard let fontDataProvider = CGDataProvider(url: fontURL as CFURL) else {
+            fatalError("Couldn't load data from the font \(fontName)")
+        }
+
+        guard let font = CGFont(fontDataProvider) else {
+            fatalError("Couldn't create font from data")
+        }
+
+        var error: Unmanaged<CFError>?
+        let success = CTFontManagerRegisterGraphicsFont(font, &error)
+        guard success else {
+            print("Error registering font: maybe it was already registered.")
+            return false
+        }
+
+        return true
+    }
+}
